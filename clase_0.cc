06dd52a9237e3d640128ccd14b63a048e97c97a7
Alejandro Méndez <209846862+00085425AlejandroMendez@users.noreply.github.com>
2025-08-14 12:35:58 -0600

    ✨ feat: Added new functions
    - New function calcularSuma()
    - New function calcularAreaCirculo()

--- clase_0.cc
+++ clase_0.cc
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cmath>
+
+const float kPi = 3.1416;
+
+int calcularSuma(int x, int y);
+
+float calcularAreaCirculo(float r);
+
+int calcularSuma(int x, int y){
+    return x + y;
+}
+
+
+float calcularAreaCirculo(float r){
+    float area = kPi * pow(r, 2);
+    return round(area * 100) / 100;
+}
 
 int main(){
+
+  int radio;
+
+  std::cout << "Ingrese el radio del circulo: ";
+  std::cin >> radio;
+
+  float area = calcularAreaCirculo(radio);
+  std::cout << "El area del circulo es: " << area << std::endl;
+
+
 
   return 0;
 }
