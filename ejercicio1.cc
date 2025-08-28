af6aefedeb9cb418a8b23eefe1be99d7b7af3fd0
Alejandro Méndez <209846862+00085425AlejandroMendez@users.noreply.github.com>
2025-08-28 12:43:57 -0600

    Feat: Creación de funcion createAlbum()

--- ejercicio1.cc
+++ ejercicio1.cc
@@ -1,12 +1,6 @@
 #include <iostream>
 #include <vector>
 
-struct Song
-{
-    int id;
-    std::string title;
-};
-
 struct Singer
 {
     std::string name;
@@ -18,16 +12,53 @@ struct Album
     std::string title;
     int release_year;
     int age;
-    std::vector<Song> songs;
+    std::vector<std::pair<int, std::string>> songs;
     Singer singer;
 };
 
-void createAlbum()
+void createAlbum(Album &album)
 {
- 
+    Singer singer;
+
+    std::cout << "Ingrese el nombre del artista: ";
+    std::cin >> singer.name;
+
+    std::cout << "Ingrese la nacionalidad del artista: ";
+    std::cin >> singer.nationality;
+
+    album.singer = singer;
+
+    std::cout << "Ingrese el titulo del album: ";
+    std::cin >> album.title;
+
+    std::cout << "Ingrese la fecha de lanzamiento del album: ";
+    std::cin >> album.release_year;
+    album.age = 2025 - album.release_year;
+
+    std::cout << "Ingrese la cantidad de canciones: ";
+    int cantidad_canciones;
+    std::cin >> cantidad_canciones;
+
+    for (int i = 0; i < cantidad_canciones; i++)
+    {
+
+        std::cout << "Ingrese el titulo de la cancion: ";
+        std::string titulo;
+        std::cin >> titulo;
+
+        album.songs.push_back(std::make_pair(i, titulo));
+    }
+
+    return;
 };
+
+
 
 int main()
 {
+
+    Album album;
+    createAlbum(album); 
+
     return 0;
 }
